2e747b9f955119c6b2928e1067c999f7d168ce91
Bedirhan Kaplan <34594057+bedirhankaplann@users.noreply.github.com>
2020-03-10 23:43:41 +0300

    KYS
    Kurum Yönetim Sistemi - Bitirme Projesi

--- KYS/App_Start/RouteConfig.cs
+++ KYS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KYS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Charts", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
